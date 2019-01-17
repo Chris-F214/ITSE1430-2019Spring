ab9fe18d0718110f21e0af50b47d438425d95aa9
michael.taylor <michael.taylor@my.tccd.edu>
2019-01-16 21:44:22 -0600

    Added first solution. Introduced basic C# syntax.

---
+++ ClassWork/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Multi line comment
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    // Single line comment
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            string name;
+            int hours = 8; //8L
+            double payRate = 8.25; //8.25F
+            int length = 10, width = 12;
+            int aReallyLongIdentifierJustToSeeHowLongICanGo;
+            char ch = 'X';
+            bool result = true; //false
+            //int counter;
+
+            //Never!!!!
+
+            //int a, b;            
+            Console.WriteLine(hours);
+            //name = "Sue";
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+
+            //string message = "Hello " + name;
+            string message = "Hello ";
+            message += name;
+
+            Console.WriteLine(message);
+            //Console.Write("Hello ");
+            //Console.WriteLine(name);
+        }
+    }
+}
