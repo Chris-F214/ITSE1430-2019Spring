e564440bc40292d1f91e8278054e02828195f1ee
michael.taylor <michael.taylor@my.tccd.edu>
2019-01-23 21:24:21 -0600

    Started work on game manage. Added new game and display game.

--- ClassWork/HelloWorld/HelloWorld/Program.cs
+++ ClassWork/HelloWorld/HelloWorld/Program.cs
@@ -1,5 +1,6 @@
 ﻿/*
- * Multi line comment
+ * Lab 1
+ * Your Name
  */
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,12 @@ namespace HelloWorld
     class Program
     {
         static void Main( string[] args )
+        {
+            NewGame();
+            DisplayGame();
+        }
+
+        private static void CSharpBasics()
         {
             string name;
             int hours = 8; //8L
@@ -39,5 +46,81 @@ namespace HelloWorld
             //Console.Write("Hello ");
             //Console.WriteLine(name);
         }
+
+        private static void NewGame ()
+        {
+            Console.WriteLine("Enter the name: ");
+            name = Console.ReadLine();
+
+            //Console.WriteLine("Do you own this? ");
+            //string owned = Console.ReadLine();
+            owned = ReadBoolean("Owned (Y/N)?");
+
+            //Console.WriteLine("Price? ");
+            //string price = Console.ReadLine();
+            price = ReadDecimal("Price?");
+
+            Console.WriteLine("Publisher? ");
+            publisher = Console.ReadLine();
+
+            //Console.WriteLine("Completed? ");
+            //string completed = Console.ReadLine();
+            completed = ReadBoolean("Completed (Y/N)?");
+        }
+
+        private static void DisplayGame ()
+        {
+            string literal1 = "Hello \"Bob\"";
+            string path = "C:\\Windows\\System32";
+            path += "\\Temp";
+            string path2 = @"C:\Windows\System32";
+
+            Console.WriteLine("Name" + name);
+            Console.WriteLine("Price: " + price);
+            Console.WriteLine("Publisher: " + publisher);
+            Console.WriteLine("Owned? " + owned);
+            Console.WriteLine("Completed? " + completed);            
+        }
+
+        private static bool ReadBoolean ( string message )
+        {
+            Console.WriteLine(message);
+            string result = Console.ReadLine();
+
+            //Validate it is a boolean
+            //HACK: Fix this expression
+            if (result == "Y")
+                return true;
+            if (result == "y")
+                return true;
+            if (result == "n")
+                return false;
+            if (result == "N")
+                return false;
+
+            //TODO: Add validation
+            return false;
+        }
+
+        //bool TryParse( string input, out decimal result );
+
+        private static decimal ReadDecimal ( string message )
+        {
+            Console.WriteLine(message);
+            string value = Console.ReadLine();
+
+            //decimal result;
+            ///if (Decimal.TryParse(value, out result))            
+            if (Decimal.TryParse(value, out decimal result))
+                return result;
+
+            return 0;
+        }
+
+        private static string name;
+        private static string publisher;
+        private static decimal price;
+        private static bool owned;
+        private static bool completed;
     }
 }
