2d985b4c8cfe3666c7d8a07494892f439f09ac32
michael.taylor <michael.taylor@my.tccd.edu>
2019-02-06 21:27:50 -0600

    Added menu to UI. Implemented File\Exit and basic Help\About. Added Validate method to Game.

--- ClassWork/GameManager/GameManager.Host.Winforms/MainForm.cs
+++ ClassWork/GameManager/GameManager.Host.Winforms/MainForm.cs
@@ -25,6 +25,29 @@ namespace GameManager.Host.Winforms
 
             game.Name = "DOOM";
             game.Price = 59.99M;
+
+            //Validate(game)
+            game.Validate();
+
+            //var x = 10;
+            //x.ToString();
+
+            //var str = game.Publisher;            
+            //Decimal.TryParse("45.99", out game.Price);
+            
+        }
+
+        private void OnFileExit ( object sender, EventArgs e )
+        {
+            //Local variable
+            var x = 10;
+
+            Close();
+        }
+
+        private void OnHelpAbout ( object sender, EventArgs e )
+        {
+            MessageBox.Show("Help");
         }
     }
 }
