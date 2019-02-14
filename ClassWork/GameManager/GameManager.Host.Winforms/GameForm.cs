7f084edfb44e1efff768c233dc8c01d7e500ff52
michael.taylor <michael.taylor@my.tccd.edu>
2019-02-13 21:43:24 -0600

    Added support for add/edit/delete for games. Added support for help/about.

--- ClassWork/GameManager/GameManager.Host.Winforms/GameForm.cs
+++ ClassWork/GameManager/GameManager.Host.Winforms/GameForm.cs
@@ -1,20 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GameManager.Host.Winforms
 {
+    /// <summary>Allows adding or editing a game.</summary>
     public partial class GameForm : Form
     {
         public GameForm()
         {
             InitializeComponent();
+        }
+
+        /// <summary>Gets or sets the property being edited.</summary>
+        public Game Game { get; set; }
+
+        //Called when the user saves the game
+        private void OnSave( object sender, EventArgs e )
+        {
+            Game = SaveData();
+
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        //Called when the user cancels the add/edit
+        private void OnCancel( object sender, EventArgs e )
+        {
+            DialogResult = DialogResult.Cancel;             
+            Close();
+        }
+
+        private decimal ReadDecimal ( TextBox control )
+        {
+            if (Decimal.TryParse(control.Text, out var value))
+                return value;
+
+            return -1;
+        }
+
+        //Loads UI with game
+        private void LoadData ( Game game )
+        {
+            _txtName.Text = game.Name;
+            _txtPublisher.Text = game.Publisher;
+            _txtPrice.Text = game.Price.ToString();
+            _cbOwned.Checked = game.Owned;
+            _cbCompleted.Checked = game.Completed;
+        }
+
+        //Saves UI into new game
+        private Game SaveData ()
+        {
+            var game = new Game();
+            game.Name = _txtName.Text;
+            game.Publisher = _txtPublisher.Text;
+            game.Price = ReadDecimal(_txtPrice);
+            game.Owned = _cbOwned.Checked;
+            game.Completed = _cbCompleted.Checked;
+
+            return game;
+        }
+
+        private void GameForm_Load( object sender, EventArgs e )
+        {
+            //Init UI if editing a game
+            if (Game != null)
+                LoadData(Game);
         }
     }
 }
